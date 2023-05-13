42b5b9f7cbcf9889aaefa46f1666b8561c16eeaa
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-05-13 16:30:31 -0300

    Create TP1.h

---
+++ OperacionesArimeticas/TP1.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAXIMO 10
+
+static int ejecucion = 0;
+
+int funcionSumar (int sumando1, int sumando2);
+int funcionRestar(int minuendo, int sustraendo);
+int funcionMultiplicacion(int multiplicando, int multiplicador);
+int funcionDividir(int dividiendo, int divisor);
+int doubleMultiplicacion(double multiplicando, double multiplicador);
+int doubleRestar(double minuendo, double sustraendo);
+int doubleSumar(double sumando1, double sumando2);
+int doubleDividir(double dividiendo, double divisor);
