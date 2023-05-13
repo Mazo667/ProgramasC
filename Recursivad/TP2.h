709b4deac9924ae8c18bc9985143411b8d2f2da8
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-05-13 16:44:18 -0300

    Create TP2.h

---
+++ Recursivad/TP2.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAXIMO 20
+
+int menu(int opcion);
+int Vocales();
+int ContarVocales(char cadena[MAXIMO], int i,int tamano);
+int Palindromo();
+int BuscarPalindromo(char cadena[],int i, int j);
+int Potencia();
+int CalculoPotencia(int numero, int exponente);
+int SumaArreglo();
+int suma_arreglo(int array[], int numero, int i);
